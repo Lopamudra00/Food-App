6af87269e2c1716b35814bdeaeb234331370891d
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2023-11-04 21:42:02 +0530

    header component styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,38 +5,36 @@ const Title = () => {
     return (
         // anchor tag to go to the home page on clicking the logo
         <a href='/'>
-            <img className='logo' src={require('../../img/food-logo.png')} />
+            <img className="w-32 h-20 p-1" src={require('../../img/food-logo.png')} alt='Dine Devine' />
         </a>
     )
 };
-//inline style. work as a object 
-const styleObj = {
-    border: '1px solid red',
-}
+
 const Header = () => {
     const [isLoggedIn, setLoggedIn] = useState(false);
 
     return (
-        <div className='header'
-            style={styleObj}
-        >
+        <div className="h-20 w-full	 flex justify-between bg-amber-500 shadow-xl ">
             <Title />
-            <div className='navItems'>
-                <ul>
-                    <Link to='/'><li>Home</li> </Link>
-                    <Link to="/about"><li>About</li></Link>
-                    <Link to="/contact"><li>Contact Us</li></Link>
-                    <li>Cart</li>
-                    <Link to="/instamart"><li>Instamart</li></Link>
+            <div className='items-center mt-7'>
+                <ul className='flex justify-between'>
+                    <Link to='/'><li className='px-3 font-sans hover:font-serif font-semibold'>Home</li> </Link>
+                    <Link to="/about"><li className='px-3 font-sans hover:font-serif font-semibold'>About</li></Link>
+                    <Link to="/contact"><li className='px-3 font-sans hover:font-serif font-semibold'>Contact Us</li></Link>
+                    <Link> <li className='px-3 font-sans hover:font-serif font-semibold'>  Cart</li></Link>
+
+                    <Link to="/instamart"><li className='px-3 font-sans hover:font-serif font-semibold'>Instamart</li></Link>
                 </ul>
             </div>
 
             {
-                (isLoggedIn) ? <button onClick={() => {
-                    setLoggedIn(false);
-                }}>Log out</button> : <button onClick={() => {
-                    setLoggedIn(true)
-                }}>Log in</button>
+                (isLoggedIn) ? <button className='mr-10 font-semibold font-sans h-12 w-24 rounded-3xl  mt-4 py-1 bg-amber-200 p-3'
+                    onClick={() => {
+                        setLoggedIn(false);
+                    }}>Log out</button> : <button className='mr-10 font-semibold font-sans h-12 w-24 rounded-3xl mt-4 py-1 bg-amber-200 p-3'
+                        onClick={() => {
+                            setLoggedIn(true)
+                        }}>Log in</button>
             }
 
 
